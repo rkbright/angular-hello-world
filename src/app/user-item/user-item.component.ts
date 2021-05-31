7a36c9a961e6ec75ed5c1b56fe9fc048a59578c0
richard bright <rkbright1@gmail.com>
2021-05-31 12:24:22 -0700

    render list

--- src/app/user-item/user-item.component.ts
+++ src/app/user-item/user-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-user-item',
@@ -6,13 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-item.component.css']
 })
 export class UserItemComponent implements OnInit {
-  name: string
+  @Input() name: string;
 
   constructor() {
-    this.name = 'Felipe!';
   }
 
-  ngOnInit(): void {
+  ngOnInit() {
   }
 
 }
